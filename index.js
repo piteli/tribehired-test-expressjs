4efbeb79e8de4b31d02e326991189a4f06264a74
piteli <rushwender@gmail.com>
2019-09-05 10:55:49 +0800

    updated

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
 const express = require('express');
 const path = require('path');
-const generatePassword = require('password-generator');
+const firebase = require("firebase");
+const firebase_config = {
+    apiKey : "AIzaSyBjoD6CzgATxsX2P3gOrJmxCVbSH-sZLq0",
+    authDomain : "tribehired-test.firebaseapp.com",
+    databaseURL : "https://tribehired-test.firebaseio.com",
+    storageBucket : ""
+};
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.json());
+app.use(express.urlencoded());
 
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
+app.post('/api/auth/login', (req, res) => {
+    const user_data = req.body;
 
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
+    if (!firebase.apps.length)firebase.initializeApp(firebase_config);
+    firebase.database().ref('/users/').orderByChild('email').equalTo(user_data.email).once('value').then((snapshot) => {
+        if(snapshot.val() == null || snapshot.val() == undefined){
+            return res.json({success : false , message : "Invalid email or password. Please try again"});
+        }
+        firebase.database().ref('/users/').orderByChild('password').equalTo(user_data.password).once('value').then((snapshot2) => {
+            return res.json({success : true , message : "Success!"});
+        })
+    })
+  });
 
-  // Return them as json
-  res.json(passwords);
+  app.post('/api/auth/register', (req, res) => {
+    const user_data = req.body;
 
-  console.log(`Sent ${count} passwords`);
-});
+    if (!firebase.apps.length)firebase.initializeApp(firebase_config);
+    firebase.database().ref('/users/').push().set({
+        username: user_data.username,
+        email: user_data.email,
+        password : user_data.password
+      }).then((snapshot) => {
+        return res.json({success : true , message : "Success!"}); 
+    }).catch((err) => {
+        return res.json({success : false , message : err}); 
+    })
+  });
+
+  app.get('/api/users/fetch', (req, res) => {
+    if (!firebase.apps.length)firebase.initializeApp(firebase_config);
+    firebase.database().ref('/users/').orderByChild('username').equalTo(req.query.search).once('value').then((snapshot) => {
+        let array = [];
+        for(var key in snapshot.val()){
+            array.push(snapshot.val()[key]);
+        }
+        return res.json(array); 
+    }).catch(() => {
+        return res.json([]);
+    })
+  });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
